591fa11fa98a253553061019f355988e29991a9b
Lucas Carvalho <lucascarvalhouibai@gmail.com>
2020-10-24 15:55:24 -0300

    Finished Web aplication with React and using data provided by find_my_coffee_api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect, useState } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import Establishment from './components/establishment';
+import EstablishmentsService from './services/establishment_service';
+import StoreService from './services/store';
+import NearstCoffees from './components/establishment/nearestCoffees';
+
+const App = () => {
+
+  const [latitude,setLatitude] = useState(0);
+  const [longitude,setLongitude] = useState(0);
+  const [locations, setLocations] = useState([]);
+  const [selected, setSelected] = useState({});
+
+  const { REACT_APP_GOOGLE_API_KEY } = process.env;
+  
+  useEffect( () => {
+    setCurrentLocation()
+  }, []);
+
+  async function loadCoffeeShops() {
+    const response = await EstablishmentsService.index(latitude, longitude);
+    setLocations(response.data.results);
+  }
+
+  async function setCurrentLocation() {
+    try {
+      await navigator.geolocation.getCurrentPosition(function (position) {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      });
+      loadCoffeeShops();
+    } catch (error) {
+      alert('Habilite a localização para utilizar o aplicativo!');
+    }
+  }
+
+  return(
+    <Fragment>
+      <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_API_KEY}>
+      <GoogleMap mapContainerStyle={{ height: "100vh", width: "100%" }}
+        zoom={15}
+        center={(selected.geometry) ? undefined : { lat: latitude, lng: longitude }}>
+        {
+          locations.map((item, index) => {
+            return (
+              <Marker key={index} icon='/images/coffee-pin.png' title={item.name} animation="4"
+                position={{ lat: item.geometry.location.lat, lng: item.geometry.location.lng }}
+                onClick={() => setSelected(item)}
+              />
+            )
+          })
+        }
+        {
+          selected.place_id && (
+            <Establishment place={selected} />
+          )
+        }
+        <Marker key='my location' icon='/images/my-location-pin.png' title="Seu Local" animation="2"
+          position={{ lat: latitude, lng: longitude }}
+        />
+        {(latitude != 0 && longitude != 0) &&
+          <NearstCoffees latitude={latitude} longitude={longitude}/>
+        }
+      </GoogleMap>
+      </LoadScript>
+    </Fragment>
   );
 }
 
